9f1984fccaaad49aeb8f3cce8e955b5da0e1b510
you <you@example.com>
2024-10-17 09:09:39 -0700

    mod5 refactor main file name, main class name, composable button widget

--- src/main/kotlin/Mod5Main.kt
+++ src/main/kotlin/Mod5Main.kt
@@ -1,4 +1,6 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -11,21 +13,30 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 @Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun Mod5AppFace() {
+    var text by remember { mutableStateOf("Hello, Mod5AppFace World!") }
 
+    Button(onClick = {
+        text = "Hello, Mod5AppFace Desktop!"
+    }) {
+        Text(text)
+    }
+}
+
+@Composable
+@Preview
+fun Mod5App() {
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            Row {
+                Mod5AppFace()
+            }
         }
     }
 }
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        Mod5App()
     }
 }
